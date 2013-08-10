9f297e1dbed4455e7012fd04033ad745e74f587b
Chris Friel <cfriel@gmail.com>
2013-08-09 21:40:36 -0400

    adding functionality for activities

--- client/create.js
+++ client/create.js
@@ -8,3 +8,14 @@ Meteor.Router.add({
 Template.create_page.rendered = function(){
     $('#left-navbar').affix();
 }
+
+Template.create_page.created = function()
+{
+    var title = "Create page loaded";
+    var description = "The created event of the create page was called";
+    var source = "Template.create_page";
+    var audience = "";
+    var activity = new Greenlight.Activity(title, description, source, audience);
+
+    activity.save();
+}
--- client/template_item.js
+++ client/template_item.js
@@ -2,7 +2,17 @@ Template.site_template_item.events({
     'click .site_template_item' : function(e,t)
     {
 	$('.site_template_item').removeClass("selected");
-	$(e.target).addClass("selected");
+	$(e.target).parent().addClass("selected");
 	console.log(t);
     }
 });
+
+Template.site_template_item.rendered = function()
+{
+    Holder.run();
+}
+
+Template.site_template_item.description = function()
+{
+    return '';
+}

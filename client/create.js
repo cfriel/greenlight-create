09d7a008d774d5db00999debcb8ed3dde3aac8c4
Chris Friel <cfriel@gmail.com>
2013-08-10 22:34:23 -0400

    restricting datasets

--- client/create.js
+++ client/create.js
@@ -15,7 +15,7 @@ Template.create_page.created = function()
     var description = "The created event of the create page was called";
     var source = "Template.create_page";
     var audience = "";
-    var activity = new Greenlight.Activity(title, description, source, audience);
+    var activity = new Greenlight.Activity({title:title, description:description, source:source, audience:audience});
 
     activity.save();
 }
